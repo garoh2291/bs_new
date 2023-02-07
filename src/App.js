a5ec318bad1fb454aabbac9f4784aa554da97ac2
garoh2291 <garoh2291@gmail.com>
2023-02-07 19:03:30 +0400

    starting project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from "styled-components";
+import { StyledApp } from "./components/styles/app.styles";
+import { GlobalStyles } from "./components/styles/Global";
+import { BrowserRouter } from "react-router-dom";
+
+const theme = {
+  colors: {
+    header: "rgb(var(--tmdbDarkBlue))",
+    body: "#fff",
+    footer: "#003333",
+  },
+  mobile: "980px",
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <StyledApp></StyledApp>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
